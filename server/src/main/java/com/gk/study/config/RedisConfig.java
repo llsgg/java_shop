463e82aec5802c918c2b560c141621984b7465aa
test <abc@163.com>
2024-05-08 23:05:39 +0800

    安装redis

---
+++ server/src/main/java/com/gk/study/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.gk.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public DefaultRedisScript<Boolean> script() {
+        DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+        // lock.lua脚本位置和application.yml同级目录
+        redisScript.setLocation(new ClassPathResource("lock.lua"));
+        redisScript.setResultType(Boolean.class);
+        return redisScript;
+    }
+}
---
+++ server/src/main/java/com/gk/study/controller/SeckillController.java
@@ -0,0 +1,18 @@
+package com.gk.study.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 秒杀
+ *
+ * @author longlin
+ * @version 1.0
+ * Create by 2024/5/7 22:23
+ */
+@Controller
+@RequestMapping("seckill")
+@Slf4j
+public class SeckillController {
+}
